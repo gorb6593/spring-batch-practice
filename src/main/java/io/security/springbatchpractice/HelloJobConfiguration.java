b10d101827f22bc6219296c32f43b8c3a0c0eaf3
haekyu.lee <haekyu.lee@readnumber.com>
2024-11-03 22:26:36 +0900

    버전이 안맞음..다시알아보기

---
+++ src/main/java/io/security/springbatchpractice/HelloJobConfiguration.java
@@ -0,0 +1,14 @@
+package io.security.springbatchpractice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+}
