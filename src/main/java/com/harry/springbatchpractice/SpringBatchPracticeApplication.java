4ba3afcbc57cdc9f5fcf3fe3fdc36cfebbe57111
harry <gorb6593@gmail.com>
2024-12-16 23:15:12 +0900

    first commit

---
+++ src/main/java/com/harry/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.harry.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeApplication.class, args);
+    }
+
+}
