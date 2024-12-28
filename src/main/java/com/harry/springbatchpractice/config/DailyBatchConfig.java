77e137b7cceb97c60205ecccff96b6ccbfc27e3a
harry <gorb6593@gmail.com>
2024-12-28 23:35:08 +0900

    일단 마무리 todo 스케줄 만들기

---
+++ src/main/java/com/harry/springbatchpractice/config/DailyBatchConfig.java
@@ -0,0 +1,15 @@
+package com.harry.springbatchpractice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * todo
+ * 스케줄 만들기
+ */
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class DailyBatchConfig {
+}
